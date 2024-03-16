034d492543dcbc3705edcf42af88eb125e66e371
Pedro Gonzalez <orion@Pedros-MacBook-Pro.local>
2024-03-16 12:15:20 -0500

    added to server.js file to parse data sent through JSON.

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const fs = require('fs');
 const path = require('path');
 const db = require('./db/db.json');
 const util = require('util');
+const { ok } = require('assert');
 const readFile = util.promisify(fs.readFile);
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(express.json());
 
+app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -22,12 +23,17 @@ app.get('/api/notes', (req, res) => {
 //create a route for '/api/notes' that will post the data of the db.json file
 app.post
 ('/api/notes', (req, res) => {
-    const newNote = req.body;
-    newNote.id = db.length.toString();
-    // fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
-    //     if (err) throw err;
-    //     res.json(db);
-    // })
+    getNotes().then(oldNotes =>{
+        const newNote = req.body;
+        var id = db.length.toString();
+        const parseNotes = [].concat(JSON.parse(oldNotes), {...newNote, id});
+        fs.writeFile('./db/db.json', JSON.stringify(parseNotes), (err) => {
+            if (err) throw err;
+            res.json({msg:"ok"});
+        })
+    })
+    
+
 })
 
 // get route for /notes that will return the notes.html localhost:3001/notes
