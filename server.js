3dee27d1ea05a90c2eb7841995c22367e7971de3
Pedro Gonzalez <orion@Pedros-MacBook-Pro.local>
2024-03-16 11:59:20 -0500

    modified server.js file to include writeFile method to convert into JSON.

--- server.js
+++ server.js
@@ -2,30 +2,33 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const db = require('./db/db.json');
-
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
+app.use(express.json());
 
+app.use(express.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended: true}))
-app.use(express.static('public'))
 
+const getNotes = () => {
+    return readFile(path.join(__dirname, './db/db.json'), 'utf8')
+}
 //create a route for '/api/notes' that will return the data of the db.json file
 app.get('/api/notes', (req, res) => {
-    res.json(db)
+    getNotes().then((data) => res.json(JSON.parse(data)));
 })
 
 //create a route for '/api/notes' that will post the data of the db.json file
 app.post
-// ('/api/notes', (req, res) => {
-//     const newNote = req.body;
-//     newNote.id = db.length.toString();
-//     db.push(newNote);
-//     fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
-//         if (err) throw err;
-//         res.json(db);
-//     })
-// })
+('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = db.length.toString();
+    // fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+    //     if (err) throw err;
+    //     res.json(db);
+    // })
+})
 
 // get route for /notes that will return the notes.html localhost:3001/notes
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
